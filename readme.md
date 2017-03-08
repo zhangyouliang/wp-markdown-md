68bbbe387e1a83bf4d63dea1af1f97983f977642
zyl <1577121881@qq.com>
2017-03-08 09:33:47 +0800

    修改转义问题

--- readme.md
+++ readme.md
@@ -8,5 +8,7 @@ wp-markdown-md
 
 ![](http://oj74t8laa.bkt.clouddn.com/git/wp-markdown-md/example.png)
 
+![](http://oj74t8laa.bkt.clouddn.com/git/wp-markdown-md/example2.gif)
+
 
 官方地址:https://pandao.github.io/editor.md/
--- wp-markdown-md.php
+++ wp-markdown-md.php
@@ -8,15 +8,30 @@
  * Website: http://www.whatdy.com
  * License:
  */
+// Make sure we don't expose any info if called directly
+if (!function_exists('add_action')) {
+    echo 'Hi there!  I\'m just a plugin, not much I can do when called directly.';
+    exit;
+}
+
+if (!function_exists('jetpack_require_lib')) {
+    include_once dirname(__FILE__) . '/jetpack/require-lib.php';
+}
+
+if (!class_exists('WPCom_Markdown')) {
+    include_once dirname(__FILE__) . '/jetpack/markdown/easy-markdown.php';
+}
+
 define('PLUGIN_VERSION', '1.0');
 
-class WpMarkdownEditor{
+class WpMarkdownEditor
+{
 
     private static $instance;
 
     public function __construct()
     {
-        register_activation_hook(__FILE__,array($this,'plugin_activation'));
+        register_activation_hook(__FILE__, array($this, 'plugin_activation'));
         register_deactivation_hook(__FILE__, array($this, 'plugin_deactivation'));
 
         // Load markdown editor
@@ -26,38 +41,56 @@ class WpMarkdownEditor{
         // Remove quicktags buttons
         add_filter('quicktags_settings', array($this, 'quicktags_settings'), $editorId = 'content');
 
+        //edit default insert images
+        add_filter('image_send_to_editor', 'new_image_send_to_editor', 21, 8);
+
         // Load Jetpack Markdown module
         $this->load_jetpack_markdown_module();
 
     }
-    public static function getInstance(){
+
+    public static function getInstance()
+    {
         if (!isset(self::$instance)) {
             $c = __CLASS__;
             self::$instance = new $c;
         }
         return self::$instance;
     }
+
     public function __clone()
     {
         trigger_error('Clone is not allowed.', E_USER_ERROR);
     }
-        function plugin_activation()
+
+    function plugin_activation()
     {
         global $wpdb;
         $wpdb->query("UPDATE `" . $wpdb->prefix . "usermeta` SET `meta_value` = 'false' WHERE `meta_key` = 'rich_editing'");
     }
+
+    /*markdown */
+    function new_image_send_to_editor($html, $id, $caption, $title, $align, $url, $size, $alt)
+    {
+        $pattern = '<img.*?src="(.*?)">';
+        preg_match($pattern, $html, $matches);
+        return "![$alt]($matches[1])";
+    }
+
     function plugin_deactivation()
     {
         global $wpdb;
         $wpdb->query("UPDATE `" . $wpdb->prefix . "usermeta` SET `meta_value` = 'true' WHERE `meta_key` = 'rich_editing'");
     }
-    function enqueue_stuffs(){
+
+    function enqueue_stuffs()
+    {
         // only enqueue stuff on the post editor page
         if (get_current_screen()->base !== 'post') {
             return;
         }
-        wp_enqueue_script('editormd-js',$this->plugin_url('/_inc/js/editormd.min.js'));
-        wp_enqueue_script('jquery-js',$this->plugin_url('/_inc/js/jquery.min.js'));
+        wp_enqueue_script('editormd-js', $this->plugin_url('/_inc/js/editormd.min.js'));
+        wp_enqueue_script('jquery-js', $this->plugin_url('/_inc/js/jquery.min.js'));
         wp_enqueue_style('editormd-css', $this->plugin_url('/_inc/css/editormd.min.css'));
     }
 
@@ -66,7 +99,8 @@ class WpMarkdownEditor{
         return plugins_url('wp-markdown-md/' . $path);
     }
 
-    function init_editor(){
+    function init_editor()
+    {
         if (get_current_screen()->base !== 'post') {
             return;
         }
@@ -77,20 +111,51 @@ class WpMarkdownEditor{
                         width   : "100%",
                         height  : 888,
                         syncScrolling : "single",
-                        path    : "'.$this->plugin_url('/_inc/lib/').'"
+                        path    : "' . $this->plugin_url('/_inc/lib/') . '"
                     });
             
                 });
         </script>';
     }
-    function quicktags_settings($qtInit){
+
+    function quicktags_settings($qtInit)
+    {
         $qtInit['buttons'] = ' ';
         return $qtInit;
     }
 
     private function load_jetpack_markdown_module()
     {
+        // If the module is active, let's make this active for posting, period.
+        // Comments will still be optional.
+        add_filter('pre_option_' . WPCom_Markdown::POST_OPTION, '__return_true');
+        add_action('admin_init', array($this, 'jetpack_markdown_posting_always_on'), 11);
+        add_action('plugins_loaded', array($this, 'jetpack_markdown_load_textdomain'));
+        add_filter('plugin_action_links_' . plugin_basename(__FILE__), array($this, 'jetpack_markdown_settings_link'));
+    }
+
+    function jetpack_markdown_posting_always_on()
+    {
+        global $wp_settings_fields;
+        if (isset($wp_settings_fields['writing']['default'][WPCom_Markdown::POST_OPTION])) {
+            unset($wp_settings_fields['writing']['default'][WPCom_Markdown::POST_OPTION]);
+        }
+    }
+
+    function jetpack_markdown_load_textdomain()
+    {
+        load_plugin_textdomain('jetpack', false, dirname(plugin_basename(__FILE__)) . '/jetpack/languages/');
+    }
+
+    function jetpack_markdown_settings_link($actions)
+    {
+        return array_merge(
+            array('settings' => sprintf('<a href="%s">%s</a>', 'options-discussion.php#' . WPCom_Markdown::COMMENT_OPTION, __('Settings', 'jetpack'))),
+            $actions
+        );
+        return $actions;
     }
 
 }
+
 WpMarkdownEditor::getInstance();
